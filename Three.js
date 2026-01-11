93336edc523bceef17627e5d6b4b139fb438081e
adam2011mhamdi-droid <adam2011mhamdi@gmail.com>
2026-01-11 20:25:21 +0100

    Create Three.js

---
+++ Three.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Fortnite 3D Simplifié</title>
+<style>
+  body { margin:0; overflow:hidden; }
+</style>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.min.js"></script>
+<script src="game3d.js"></script>
+</body>
+</html>
